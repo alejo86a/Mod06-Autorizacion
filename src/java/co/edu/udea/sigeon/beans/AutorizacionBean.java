65e85d01e731325629727577cc57a13df0e97f8f
alejo86a <alejo86a@hotmail.com>
2015-02-03 13:10:32 -0500

    v0 commit inicial

---
+++ src/java/co/edu/udea/sigeon/beans/AutorizacionBean.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package co.edu.udea.sigeon.beans;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author alejandro
+ */
+@ManagedBean
+@SessionScoped
+public class AutorizacionBean {
+
+    /**
+     * Creates a new instance of AutorizacionBean
+     */
+    public AutorizacionBean() {
+    }
+    
+}
